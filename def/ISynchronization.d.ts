7b556367737026bdbf3d61853fc66b635e482911
Sefa Ilkimen <silkimen@users.noreply.github.com>
2023-03-13 03:04:23 +0100

    feat: MSB-5903 - support uploading and downloading all application tables without providing all table names

--- def/ISynchronization.d.ts
+++ def/ISynchronization.d.ts
@@ -1,6 +1,6 @@
 interface ISynchronization {
   initialize(callback: (error?: IMsbAppError) => void): void;
-  download(tableName: string, callback: (error?: IMsbAppError) => void): void;
-  upload(tableName: string, callback: (error?: IMsbAppError) => void): void;
+  download(tableName: string | string[] | null | undefined, callback: (error?: IMsbAppError) => void): void;
+  upload(tableName: string | string[] | null | undefined, callback: (error?: IMsbAppError) => void): void;
   uploadFiles(callback: (error?: IMsbAppError) => void): void;
 }
