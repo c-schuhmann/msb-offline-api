e71de91eae3b130eb2f55dff613d254b42003fca
Sefa Ilkimen <silkimen@users.noreply.github.com>
2023-03-13 14:25:16 +0100

    Update def/ISynchronization.d.ts
    Co-authored-by: Robin Hartmann <robin.hartmann@mobisys.de>

--- def/ISynchronization.d.ts
+++ def/ISynchronization.d.ts
@@ -1,6 +1,6 @@
 interface ISynchronization {
   initialize(callback: (error?: IMsbAppError) => void): void;
-  download(tableName: string | string[] | null | undefined, callback: (error?: IMsbAppError) => void): void;
-  upload(tableName: string | string[] | null | undefined, callback: (error?: IMsbAppError) => void): void;
+  download(tableNames: string | string[] | null | undefined, callback: (error?: IMsbAppError) => void): void;
+  upload(tableNames: string | string[] | null | undefined, callback: (error?: IMsbAppError) => void): void;
   uploadFiles(callback: (error?: IMsbAppError) => void): void;
 }
